d1b3fb5a4f0dc96bbf6b4ec948ad5d524b478e9d
Seonwoo Kim <177065@jnu.ac.kr>
2020-07-24 21:24:56 +0900

    Updates

---
+++ precache-manifest.e20b5ba0bce94bc6046e8a01c387e88b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc1f3b8393ae3f87b7f551a599e166eb",
+    "url": "/react_wedding/index.html"
+  },
+  {
+    "revision": "70a24a837c99d7cb7609",
+    "url": "/react_wedding/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "932d7dc7296c13275083",
+    "url": "/react_wedding/static/js/2.0717a295.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_wedding/static/js/2.0717a295.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70a24a837c99d7cb7609",
+    "url": "/react_wedding/static/js/main.42681765.chunk.js"
+  },
+  {
+    "revision": "23c3e5d9364738577eef",
+    "url": "/react_wedding/static/js/runtime-main.926d1d21.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_wedding/static/media/logo.5d5d9eef.svg"
+  }
+]);
